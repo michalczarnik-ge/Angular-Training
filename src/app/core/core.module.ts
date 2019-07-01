a5bde0035f46617a6ffd3ea6325b9b927f794a21
Czarnik <212469721@logon.ds.ge.com>
2019-07-01 14:27:57 +0200

    Added menu and new pages

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { AppComponent } from './components/app/app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomePageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
