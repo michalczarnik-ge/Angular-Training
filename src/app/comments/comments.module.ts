1474ff4487a26354ba053df330d7c4ea69d38192
Czarnik <212469721@logon.ds.ge.com>
2019-07-02 15:29:39 +0200

    Added basic code for comments

---
+++ src/app/comments/comments.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CommentListComponent } from './components/comment-list/comment-list.component';
+import { CommentListItemComponent } from './components/comment-list-item/comment-list-item.component';
+import { CommentAddFormComponent } from './components/comment-add-form/comment-add-form.component';
+
+@NgModule({
+  declarations: [CommentListComponent, CommentListItemComponent, CommentAddFormComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CommentsModule { }
---
+++ src/app/comments/components/comment-list-item/comment-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-list-item',
+  templateUrl: './comment-list-item.component.html',
+  styleUrls: ['./comment-list-item.component.scss']
+})
+export class CommentListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
