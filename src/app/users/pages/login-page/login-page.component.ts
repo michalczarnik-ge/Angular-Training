cdd36241ebf1c1ee287a2864a973b849e6507aca
Czarnik <212469721@logon.ds.ge.com>
2019-07-03 15:16:04 +0200

    Added saving loging to user storage and restoring it

--- src/app/users/pages/login-page/login-page.component.ts
+++ src/app/users/pages/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../services/users.service';
 import { Router } from '@angular/router';
+import { StorageService } from 'src/app/shared/services/storage.service';
 
 @Component({
   selector: 'app-login-page',
--- src/app/users/services/users.service.ts
+++ src/app/users/services/users.service.ts
@@ -4,6 +4,7 @@ import { IUser } from '../interfaces/user.interface';
 import { environment } from 'src/environments/environment';
 import { IUserList } from '../interfaces/user-list.interface';
 import { BehaviorSubject } from 'rxjs';
+import { StorageService } from 'src/app/shared/services/storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,11 @@ export class UsersService {
   $user = new BehaviorSubject(null);
 
   constructor(
-    private http: HttpClient
-  ) { }
+    private http: HttpClient,
+    private storage: StorageService
+  ) { 
+    this.restore();
+  }
 
   register(user: IUser) {
     this.http.post(environment.usersUrl, user).toPromise();
@@ -24,7 +28,7 @@ export class UsersService {
     const users = await this.http.get<IUserList>(environment.usersUrl).toPromise();
     const authUser = users.find((u) => {
       return u.email === email && u.password === password;
-    })
+    });
     return{
       status: Boolean(authUser),
       ...authUser
@@ -32,10 +36,20 @@ export class UsersService {
   }
 
   auth(authResponse: { id: string; name: string; avatar_url: string; email?: string; password?: string; status: boolean; }) {
+    this.storage.create('user', authResponse);
     this.$user.next(authResponse);
   }
 
   logout(){
+    this.storage.delete('user');
     this.$user.next(null);
+  }
+
+  restore(){
+    const user = this.storage.read('user');
+    if(!user){
+      return;
+    }
+    this.$user.next(user);
   }
 }
