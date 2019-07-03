a35db0535b65898ad244de9a975a2a093342fe73
Czarnik <212469721@logon.ds.ge.com>
2019-07-03 14:59:24 +0200

    Added to providers

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HighlightDirective } from './directives/highlight.directive';
 import { MyDatePipe } from './pipes/my-date.pipe';
 import { LoaderComponent } from './components/loader/loader.component';
+import { StorageService } from './services/storage.service';
 
 @NgModule({
   declarations: [HighlightDirective, MyDatePipe, LoaderComponent],
   exports: [HighlightDirective, MyDatePipe, LoaderComponent],
   imports: [
     CommonModule
-  ]
+  ], 
+  providers: [StorageService]
 })
 export class SharedModule { }
